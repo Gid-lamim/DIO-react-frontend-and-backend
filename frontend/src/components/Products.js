c8e1a16db969c9fac32570444f3077277dce4c58
Gid.Lamim <gid.lamim@gmail.com>
2022-07-27 22:31:26 -0300

    fix products card

--- frontend/src/components/Products.js
+++ frontend/src/components/Products.js
@@ -1,23 +1,26 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { Paper, Grid, Typography, List, makeStyles } from '@material-ui/core/';
-import Item from './Item';
 import Card from './Card';
 
-const Products = ()  =>{
-const products = useSelector(state => state.products)
 
+const Products = (props)  =>{
 
+    
+console.log(props.products);
+console.log('props');
 return<>
-                {products.map(item => {
+                {props.products.map(item => {
                     
                     return(
+                        <>
                         <Card
                             key={item.id_product}
                             product={item}
                         >
                             {item.name_product}
                         </Card>
+                        </>
                     )
                 })}
         </>
